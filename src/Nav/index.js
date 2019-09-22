af1fa0a85f7764738688a9e0ae60cc2535e2c556
tactnician <tactnician@gmail.com>
2019-09-22 14:00:17 -0400

    Created inital components

---
+++ src/Nav/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { 
+    faBolt,
+    faKeyboard,
+    faRocket,
+    faCode,
+    faCoffee,
+    faPlusCircle,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {  } from "@fortawesome/fontawesom"; 
+
+imp
+
+import NavBar from './styles'
+
+const Nav = () => {
+    return (
+        <NavBar>
+            <FontAwesomeIcon icon = {faBolt} />
+            <FontAwesomeIcon icon = {faCode} />
+            <FontAwesomeIcon icon = {faBolt} />
+        </NavBar>
+    )
+}
+
+export default Nav
---
+++ src/Nav/styles.js
@@ -0,0 +1,17 @@
+
+
+import styled from 'styled-components'
+
+export const NavBar = styled.nav`
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    height: 280px;
+    width: 10%;
+    padding: 0;
+    text-align: center;
+`
+
